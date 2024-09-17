51c9190779950b24155a23af21b3191dcc1b2110
Fernando Rojas <frojasq@yahoo.com>
2024-09-17 14:41:53 -0500

    Feat:Menú desktop

--- components/Shared/Navbar/MenuDesktop/MenuDesktop.tsx
+++ components/Shared/Navbar/MenuDesktop/MenuDesktop.tsx
@@ -1,3 +1,28 @@
+"use client"
+
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
 export function MenuDesktop() {
-  return <div>MenuDesktop</div>;
+  const linkBolsaTrabajo = ""
+
+  return (
+    <NavigationMenu className="z-50">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+          <NavigationMenuLink
+            className={cn(navigationMenuTriggerStyle(), "bg-transparent font-headMedium text-black text-lg")}
+          >
+            Inicio
+          </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem></NavigationMenuItem>
+        <NavigationMenuItem></NavigationMenuItem>
+        <NavigationMenuItem></NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
